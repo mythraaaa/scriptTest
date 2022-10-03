18726fddcda4cab20f27b262885afc8d174ba714
akira saitou <asdfphobos@gmail.com>
2022-10-03 16:51:32 +0900

    Update Test.cs

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,6 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Boss
+
+{
+    public int mp = 53; // MP
+    
+    // 魔法用の関数
+    public void Magic()
+    {
+        if (mp >= 5)
+        {
+            // MPが5以上の場合
+            Debug.Log("魔法攻撃をした。残りMPは〇〇");
+            // 残りmpを減らす
+            this.mp -= 5;
+        }
+        else
+        {
+            // MPが5未満の場合
+            Debug.Log("MPが足りないため、魔法が使えない。");
+        }
+        
+    }
+
+}
+
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -16,9 +41,20 @@ public class Test : MonoBehaviour
             Debug.Log(array[a]);
         }
         //3.for文を使い、配列の各要素の値を逆番に表示してください
-        for (int b = 4; b > 0; b--)
+        for (int b = 4; b > -1; b--)
         {
             Debug.Log(array[b]);
+        }
+        //発展課題
+
+            // Bossクラスの変数を宣言してインスタンスを代入
+            Boss lastboss = new Boss();
+
+            // 魔法用の関数を呼び出す
+            lastboss.Magic();
+        for(int mp = 0; mp < 11; mp++)
+        {
+            Debug.Log(mp);
         }
     }
         // Update is called once per frame
